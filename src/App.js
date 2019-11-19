16c097f24f79ceeec0efc137e6e1f251ac3d30d5
aaron <aaronakamm@gmail.com>
2019-11-19 12:47:49 +0200

    React Router for welcome message

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import Search from './components/Search';
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
-
-
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Welcome from './components/Welcome';
 
 const App = () => {
   return (
-    
-    <div className = "App">
-      <AppBar position = "static">
-        <Toolbar>
-          <Typography variant = "h4" component = "h1">Recipe Finder</Typography>
-        </Toolbar>
-      </AppBar>
-      <Search />
-    </div>
+    <Router>
+      <div className = "App">
+        <AppBar position = "static">
+          <Toolbar>
+            <Typography variant = "h4" component = "h1">Recipe Finder</Typography>
+          </Toolbar>
+        </AppBar>
+        <Search />
+        <Route path = '/' component = {Welcome} />  
+      </div>
+    </Router>
    
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Welcome = () => {
-
+  return <section><h1>Welcome</h1></section>
 }
 
 export default Welcome;
