9f000f4518791e67be021e8e1c2b4d080a592dbc
aaron <aaronakamm@gmail.com>
2019-12-04 14:43:53 +0200

    Set up folder structure and provider for Redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Search from './components/Search';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Welcome from './components/Welcome';
 import NavBar from './components/NavBar';
+import {Provider} from 'react-redux';
 
 
-const App = () => {
+
+const App = ({store}) => {
   return (
-    <Router>
-      <div className = "App">
-        <NavBar/>
-        <Search />
-        <Route path = '/' exact component = {Welcome} /> 
-      </div>
-    </Router>
+    <Provider store = {store}>
+      <Router>
+        <div className = "App">
+          <NavBar/>
+          <Search />
+          <Route path = '/' exact component = {Welcome} /> 
+        </div>
+      </Router>
+    </Provider>
    
   );
 }
