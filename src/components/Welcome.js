b3ec4e4e53b502a6dc018fd67b5f19066c491142
aaron <aaronakamm@gmail.com>
2020-02-17 21:28:39 +0200

    Changed font of homepage caption

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  welcome: {
+  homeComponent: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -14,15 +14,15 @@ const useStyles = makeStyles({
 const Welcome = () => {
   const classes = useStyles();
   return (
-    <div className={classes.welcome}>
+    <div className={classes.homeComponent}>
       <div>
-        <Typography align="center" variant="h2" component="h1">
+        <Typography align="center" style={{ fontSize: '40px' }}>
           Welcome!
         </Typography>
-        <Typography style={{ fontSize: '1rem' }} variant="subtitle1">
+        <Typography style={{ fontSize: '16px' }} variant="subtitle1">
           Get all your favorite recipes here{' '}
           <span role="img" aria-label="emojis">
-            🍔🍞😋
+            🍔😋
           </span>
         </Typography>
       </div>
